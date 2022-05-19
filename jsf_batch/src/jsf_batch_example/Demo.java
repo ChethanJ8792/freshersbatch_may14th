6dbf76f3c6562b4d9fd53036d7969864b02d08bd
ChethanJ8792 <chethan2jsr4@gmail.com>
2022-05-19 21:28:48 +0530

    First commit

---
+++ jsf_batch/src/jsf_batch_example/Demo.java
@@ -0,0 +1,11 @@
+package jsf_batch_example;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First commit");
+		
+	}
+
+}
